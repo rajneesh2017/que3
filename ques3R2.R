74e96c9ed90552d2ae85b84aa0b8020bdf4d890c
Rajneesh Kumar Pandey <rajneesh2017@gmail.com>
2014-10-29 16:53:03 -0500

    Update ques3R2.R

--- ques3R2.R
+++ ques3R2.R
@@ -3,6 +3,6 @@ data <-RCurl::getURL("https://raw.githubusercontent.com/jrwolf/IT497/master/fits
 
 extradf <- read.table(textConnection(data),sep=",",header=T)
 
-SPfor2 <- ggplot(extradf, aes(x=Steps, y=Miles)) + geom_point(shape=1)
+p1 <- ggplot(extradf, aes(x=Steps, y=Miles)) + geom_point(shape=1)
 
-SPfor1 <- ggplot(extradf, aes(x=Miles, y=Steps)) + geom_point(shape=1)
+p2 <- ggplot(extradf, aes(x=Miles, y=Steps)) + geom_point(shape=1)
